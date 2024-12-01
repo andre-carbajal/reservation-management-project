362b0121b02333bd116e014f9650ca590826b070
andre-carbajal <carbajalvargasandre@gmail.com>
2024-12-01 17:11:16 -0500

    Refactor login window UI

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import tkinter as tk
 
-def inicioSesion():
-    print("Inicio secion")
-          
+def loginUser():
+    print("Inicio sesion")
+
 login = tk.Tk()
-login.resizable(0,0)
+login.resizable(False, False)
 login.title("Login")
 
 wventana = 854
 hventana = 480
-pwidth = round(login.winfo_screenwidth()/2-wventana/2)
-pheight = round(login.winfo_screenheight()/2-hventana/2)
+pwidth = round(login.winfo_screenwidth() / 2 - wventana / 2)
+pheight = round(login.winfo_screenheight() / 2 - hventana / 2)
 
-login.geometry(str(wventana)+"x"+str(hventana)+"+"+str(pwidth)+"+"+str(pheight))
+login.geometry(f"{wventana}x{hventana}+{pwidth}+{pheight}")
 
-email = tk.Label(login, text="Ingrese su email")
-emailText = tk.Entry(login)
-email.pack()
-emailText.pack()
+email = tk.Label(login, text="Ingrese su email:")
+emailText = tk.Entry(login, width=40)
+password = tk.Label(login, text="Ingrese su contraseña:")
+passwordText = tk.Entry(login, show="*", width=40)
+loginButton = tk.Button(login, text="Login", command=loginUser)
 
-password = tk.Label(login, text="Ingrese su contraseña")
-passwordText = tk.Entry(login)
-password.pack()
-passwordText.pack()
-
-loginButton = tk.Button(login, text="Login")
-loginButton.pack()
+# Centering the content
+email.place(relx=0.5, rely=0.3, anchor='center')
+emailText.place(relx=0.5, rely=0.35, anchor='center')
+password.place(relx=0.5, rely=0.4, anchor='center')
+passwordText.place(relx=0.5, rely=0.45, anchor='center')
+loginButton.place(relx=0.5, rely=0.55, anchor='center')
 
 login.mainloop()
